8a886a739c902ca95095ed2135b7d9eeeb2c0754
BradleyVerrinder <bradleyverrinder@gmail.com>
2025-05-19 18:11:36 +0100

    Shooting and damage now syncs across network so dummy targets take damage and despawn on host and client

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,7 +1,7 @@
-using UnityEngine;
 using Unity.Netcode;
+using UnityEngine;
 
-public class Gun : MonoBehaviour
+public class Gun : NetworkBehaviour
 {
     public float damage = 25f;
     public float range = 100f;
@@ -10,11 +10,9 @@ public class Gun : MonoBehaviour
 
     private float nextTimeToFire = 0f;
 
-    
-
     void Update()
     {
-        if (!cam || !Input.GetButton("Fire1") || Time.time < nextTimeToFire) return;
+        if (!IsOwner || !cam || !Input.GetButton("Fire1") || Time.time < nextTimeToFire) return;
 
         nextTimeToFire = Time.time + 1f / fireRate;
         Shoot();
@@ -22,21 +20,43 @@ public class Gun : MonoBehaviour
 
     void Shoot()
     {
+        // Only local detection, the actual logic happens on the server
         Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         if (Physics.Raycast(ray, out RaycastHit hit, range))
         {
-            Debug.Log("Hit: " + hit.collider.name);
-            // Damage logic here
+            Vector3 hitPoint = hit.point;
+            ulong targetNetworkObjectId = 0;
 
-            // Grabs the target script from the target
-            Target target = hit.transform.GetComponentInParent<Target>();
+            NetworkObject targetNetObj = hit.transform.GetComponentInParent<NetworkObject>();
+            if (targetNetObj != null)
+                targetNetworkObjectId = targetNetObj.NetworkObjectId;
 
+            ShootServerRpc(hitPoint, targetNetworkObjectId);
+        }
+    }
 
-            // If target has the target script attached -> take damage
+    [ServerRpc]
+    void ShootServerRpc(Vector3 hitPoint, ulong targetId)
+    {
+        // Optional: validate client fire rate, etc.
+
+        if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(targetId, out NetworkObject targetObj))
+        {
+            Target target = targetObj.GetComponent<Target>();
             if (target != null)
             {
                 target.TakeDamage(damage, cam);
             }
         }
+
+        // Call ClientRpc to show effects on all clients
+        ShootClientRpc(hitPoint);
+    }
+
+    [ClientRpc]
+    void ShootClientRpc(Vector3 hitPoint)
+    {
+        // TODO: Play hit effect / muzzle flash / audio / animation on all clients
+        Debug.Log("Playing hit effect at " + hitPoint);
     }
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
+using Unity.Netcode;
 
-public class Target : MonoBehaviour
+[RequireComponent(typeof(NetworkObject))]
+public class Target : NetworkBehaviour
 {
     public float health = 50f;
+
+    private NetworkObject netObj;
     public GameObject floatingTextPrefab;
+
+
+    void Awake()
+    {
+        netObj = GetComponent<NetworkObject>();
+    }
 
     public void TakeDamage(float amount, Camera shooterCamera)
     {
+        if (!IsServer) return;
+        
         health -= amount;
         Debug.Log(gameObject.name + " took " + amount + " damage. Remaining health: " + health);
 
@@ -23,13 +35,23 @@ public class Target : MonoBehaviour
 
             FloatingText ft = instance.GetComponent<FloatingText>();
             ft.SetText(amount.ToString());
-            ft.shooterCamera = shooterCamera;  
+            ft.shooterCamera = shooterCamera;
         }
     }
+    
 
     void Die()
     {
-        Debug.Log(gameObject.name + " died.");
-        Destroy(gameObject);
+        Debug.Log($"{gameObject.name} died. IsServer: {IsServer}, IsClient: {IsClient}");
+
+        if (IsServer && netObj.IsSpawned)
+        {
+            Debug.Log("Despawning target on server.");
+            netObj.Despawn(); // This will sync to clients if scene object is correctly registered
+        }
+        else
+        {
+            Debug.LogWarning("Despawn failed: either not server or not spawned.");
+        }
     }
 }
